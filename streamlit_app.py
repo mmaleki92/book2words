8f48b9bf58184968ca2b8507d43c2024db5b614c
morteza <maleki.morteza92@gmail.com>
2021-09-10 20:21:29 +0430

    added .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 import multiprocessing
 import os
-from file_reader import read_files
+# from file_reader import read_files
 # from ocr import ocr
 import re
 from googletrans import Translator
